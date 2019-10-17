7d8cad4c594fb580fb7a3423affa27dee935dd1e
jho22 <jimmyyo5826@gmail.com>
2019-10-17 09:49:34 -0600

    creating a loading interface

---
+++ jargon-v-06/single-features.php
@@ -0,0 +1,11 @@
+<?php
+// array of the things in the group
+// $group['title']
+$group = get_field('header');	
+
+?>
+
+<header>
+    <h1>What Show Up:::::::::::::::::<?php $group['title']?></h1>
+    <h2></h2>
+</header>
