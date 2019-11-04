7b4691b872d536eec135164fa420138ea6d5d201
jho22 <jimmyyo5826@gmail.com>
2019-11-04 13:56:21 -0700

    add in login link

--- jargon-v-07/header.php
+++ jargon-v-07/header.php
@@ -10,19 +10,19 @@
 <body>
 
 <nav class="site-nav">
-       <ul class="icon-view">
-       <li>Jargon</li>
-       <li>new courses</li>
-       <li>articles</li>
-       <li>javascript</li>
-       <li>css</li>
-       <li>frameworks</li>
-       <li>builds</li>
-       <li>ui/ux</li>
-       <li >
-        <button class="login-button">login</button>
-       </li>
-       </ul>
+    <ul class="icon-view">
+        <li>Jargon</li>
+        <li>new courses</li>
+        <li>articles</li>
+        <li>javascript</li>
+        <li>css</li>
+        <li>frameworks</li>
+        <li>builds</li>
+        <li>ui/ux</li>
+        <li >
+            <a href="http://jho22.dmitstudent.ca/dmit2032/test/wp-login.php"><button class="login-button">login</button></a>
+        </li>
+    </ul>
 </nav>
 
 
