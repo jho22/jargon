5439d00655ff9c8225f0143de05a856fd408ca1b
jho22 <jimmyyo5826@gmail.com>
2019-11-10 17:19:43 -0700

    update

--- jargon-v-07/header.php
+++ jargon-v-07/header.php
@@ -12,16 +12,7 @@
 <nav class="site-nav">
     <ul class="icon-view">
         <li>Jargon</li>
-        <li>new courses</li>
-        <li>articles</li>
-        <li>javascript</li>
-        <li>css</li>
-        <li>frameworks</li>
-        <li>builds</li>
-        <li>ui/ux</li>
-        <li >
-            <a href="http://jho22.dmitstudent.ca/dmit2032/test/wp-login.php"><button class="login-button">login</button></a>
-        </li>
+        
     </ul>
 </nav>
 
--- jargon-v-07/page-templates/articles.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-    Template Name: Featured Article
-    Template Post Type: post, page, product
-*/
-
-get_header();
-?>
-
